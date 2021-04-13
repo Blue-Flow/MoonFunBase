fb8cdd81075c4aa09e7a51403c3a4a9f404e8989
Blue-Flow <77975268+Blue-Flow@users.noreply.github.com>
2021-04-13 19:26:13 +0200

    BaseGame
    Base Proto from Zenva Course + started changes to adapt to our project (resources types)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentTurn;
+    public int maxFun = 20;
+    public bool placingBuilding;
+    public BuildingType curSelectedBuilding;
+
+    [Header("Current Resources")]
+    public int currentFun;
+    public int currentMaterials;
+    public int currentOxygen;
+    public int currentEnergy;
+
+    [Header("Round Resource Increase")]
+    public int funPerTurn;
+    public int materialsPerTurn;
+    public int oxygenPerTurn;
+    public int energyPerTurn;
+
+    public static GameManager instance;
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    void Start ()
+    {
+        // updating the resource UI
+        UI.instance.UpdateResourceText();
+    }
+
+    // called when the "End Turn" button is pressed
+    public void EndTurn ()
+    {
+        // give resources
+        currentFun += funPerTurn;
+        currentMaterials += materialsPerTurn;
+        currentOxygen += oxygenPerTurn;
+        currentEnergy += energyPerTurn;
+
+        // update the resource UI
+        UI.instance.UpdateResourceText();
+
+        currentTurn++;
+
+        // enable the building buttons
+        UI.instance.ToggleBuildingButtons(true);
+
+        // enable usable tiles
+        Map.instance.EnableUsableTiles();
+    }
+
+    // called when we click on a building button to place it
+    public void SetPlacingBuilding (BuildingType buildingType)
+    {
+        placingBuilding = true;
+        curSelectedBuilding = buildingType;
+    }
+
+    // called when a new building has been created and placed down
+    public void OnCreatedNewBuilding (Building building)
+    {
+        // resource the building produces
+        if(building.doesProduceResource)
+        {
+            switch(building.productionResource)
+            {
+                case ResourceType.Food:
+                    funPerTurn += building.productionResourcePerTurn;
+                    break;
+                case ResourceType.Metal:
+                    materialsPerTurn += building.productionResourcePerTurn;
+                    break;
+                case ResourceType.Oxygen:
+                    oxygenPerTurn += building.productionResourcePerTurn;
+                    break;
+                case ResourceType.Energy:
+                    energyPerTurn += building.productionResourcePerTurn;
+                    break;
+            }
+        }
+
+        // resource the building may cost
+        if(building.hasMaintenanceCost)
+        {
+            switch(building.maintenanceResource)
+            {
+                case ResourceType.Food:
+                    funPerTurn -= building.maintenanceResourcePerTurn;
+                    break;
+                case ResourceType.Metal:
+                    materialsPerTurn -= building.maintenanceResourcePerTurn;
+                    break;
+                case ResourceType.Oxygen:
+                    oxygenPerTurn -= building.maintenanceResourcePerTurn;
+                    break;
+                case ResourceType.Energy:
+                    energyPerTurn -= building.maintenanceResourcePerTurn;
+                    break;
+            }
+        }
+
+        placingBuilding = false;
+        // update the resource UI
+        UI.instance.UpdateResourceText();
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI : MonoBehaviour
+{
+    public GameObject buildingButtons;
+
+    public TextMeshProUGUI oxygenValue;
+    public TextMeshProUGUI energyValue;
+    public TextMeshProUGUI materialsValue;
+    public Image funBar;
+
+    public TextMeshProUGUI curTurnText;
+
+    public static UI instance;
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    void Start ()
+    {
+        curTurnText.text = "Turn " + GameManager.instance.currentTurn;
+    }
+
+    // called when the "End Turn" button is pressed
+    public void OnEndTurnButton ()
+    {
+        GameManager.instance.EndTurn();
+        curTurnText.text = "Turn " + GameManager.instance.currentTurn;
+    }
+
+    // called when we select a building (disables the buttons)
+    public void ToggleBuildingButtons (bool toggle)
+    {
+        buildingButtons.SetActive(toggle);
+    }
+
+    // called when we click the solar panel button
+    public void OnClickSolarPanelButton ()
+    {
+        GameManager.instance.SetPlacingBuilding(BuildingType.SolarPanel);
+        ToggleBuildingButtons(false);
+    }
+
+    // called when we click the greenhouse button
+    public void OnClickGreenhouseButton()
+    {
+        GameManager.instance.SetPlacingBuilding(BuildingType.Greenhouse);
+        ToggleBuildingButtons(false);
+    }
+
+    // called when we click the mine button
+    public void OnClickMineButton()
+    {
+        GameManager.instance.SetPlacingBuilding(BuildingType.Mine);
+        ToggleBuildingButtons(false);
+    }
+
+    // called when we place a building or the turn has ended
+    public void UpdateResourceText ()
+    {
+        string materials = string.Format("{0} ({1}{2})", GameManager.instance.currentMaterials, GameManager.instance.materialsPerTurn < 0 ? "" : "+", GameManager.instance.materialsPerTurn);
+        string oxygen = string.Format("{0} ({1}{2})", GameManager.instance.currentOxygen, GameManager.instance.oxygenPerTurn < 0 ? "" : "+", GameManager.instance.oxygenPerTurn);
+        string energy = string.Format("{0} ({1}{2})", GameManager.instance.currentEnergy, GameManager.instance.energyPerTurn < 0 ? "" : "+", GameManager.instance.energyPerTurn);
+
+        oxygenValue.text = oxygen;
+        energyValue.text = energy;
+        materialsValue.text = materials;
+
+        funBar.fillAmount = (GameManager.instance.currentFun / GameManager.instance.maxFun);
+    }
+}
