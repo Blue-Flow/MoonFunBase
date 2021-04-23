607a99f766864b5b437c8b8be65038bad400c404
Blue-Flow <77975268+Blue-Flow@users.noreply.github.com>
2021-04-23 16:07:49 +0200

    Volume setting
    Implemented volume settings for the main mixer

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] AudioMixer audioMixer;
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -13,5 +15,10 @@ public class Menu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
     }
 }
